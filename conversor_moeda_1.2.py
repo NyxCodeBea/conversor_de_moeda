a21391372cad73d2c781386fa126024dd71fbfb6
NyxCodeBea <beatriz.amorinn@gmail.com>
2026-01-14 22:14:11 -0300

    Esta atualização focou na flexibilidade matemática e na integridade dos dados, permitindo conversões de e para a moeda nacional.

---
+++ conversor_moeda_1.2.py
@@ -0,0 +1,95 @@
+# --- 1. Configurações e Dados ---
+
+# Dicionário de Nomes (A Agenda)
+nomes_moedas = {
+    1: "Dólar Americano",
+    2: "Euro",
+    3: "Libra Esterlina",
+    4: "Yuan Chinês",
+    5: "Won sul-coreano",
+    6: "Real Brasileiro"
+}
+
+# Dicionário de Taxas (A Tabela de Preços)
+taxas_conversao = {
+    (1, 6): 5.25,    # Dólar para Real
+    (2, 6): 5.90,    # Euro para Real
+    (3, 6): 6.80,    # Libra para Real
+    (4, 6): 0.80,    # Yuan para Real
+    (5, 6): 0.0045  # Won para Real
+}
+
+# Dicionário de Símbolos
+simbolos_moedas = {
+    1: "$",   # Dólar
+    2: "€",   # Euro
+    3: "£",   # Libra
+    4: "¥",   # Yuan
+    5: "₩",   # Won
+    6: "R$"   # Real
+}
+
+# Nome da moeda de destino (Fixo no Real por enquanto)
+nomes_destino = {
+    1: "Dólar Americano",
+    2: "Euro",
+    3: "Libra Esterlina",
+    4: "Yuan Chinês",
+    5: "Won sul-coreano",
+    6: "Real Brasileiro"
+}
+
+def conversor_moeda(valor, id_origem, id_destino): 
+    # Agora a função recebe tudo o que precisa para trabalhar!
+    
+    # 1. Busca o nome da moeda e simbolos
+    nome_origem = nomes_moedas[id_origem]
+    nome_destino = nomes_destino[id_destino]
+    simbolo_origem = simbolos_moedas[id_origem]
+
+    # 2. Faz o cálculo
+    if(id_origem == 6):  # Se a origem for REAL (6), nós DIVIDIMOS para comprar moeda estrangeira
+        # 1. Busca a taxa usando a TUPLA (origem, destino)
+        # Exemplo: Se id_origem for 2 e id_destino for 1, busca (2, 1)
+        taxa = taxas_conversao[id_destino, id_origem] # Inverte a taxa para conversão de Real para moeda estrangeira 
+        valor_final = valor / taxa
+    else:
+        taxa = taxas_conversao[id_origem, id_destino]  # Mantém a taxa para conversão de moeda estrangeira para Real
+        valor_final = valor * taxa # Se a origem for ESTRANGEIRA, nós MULTIPLICAMOS para virar Real
+
+    # 4. Mostra o resultado
+    print(f"Quanto vale {simbolo_origem} {valor:.2f} {nome_origem} em {nome_destino}?")
+    print(f"Resultado: R$ {valor_final:.2f}")
+
+
+# --- 2. Recebendo as Informações do usuário ---
+
+print("Calculadora Conversor de Moeda")
+
+while True:
+    comecar = input("\nConverter Moedas? S (sim) ou N (não): ")
+    if comecar.upper() == "N":
+        print("Encerrando Calculo de conversor...\n")
+        break
+    else:
+        try:
+            # 1. Pede o VALOR e guarda na variável 'valor_usuario'
+            valor_usuario = float(input("Insira o valor a converter: "))
+
+            print("Moedas disponíveis: 1-Dólar / 2-Euro / 3-Libra / 4-Yuan / 5-Won / 6-Real")
+            
+            # 2. Pede a ORIGEM e guarda na variável 'escolha_origem'
+            escolha_origem = int(input("Moeda de origem (código): "))
+
+            # 3. Pede o DESTINO e guarda na variável 'escolha_destino'
+            print("Converter para: 1-Dólar / 2-Euro / 3-Libra / 4-Yuan / 5-Won / 6-Real")
+            escolha_destino = int(input("Moeda de destino (código): "))
+
+            # --- CHAMADA DA FUNÇÃO ---
+            # Aqui enviamos as 3 informações que coletamos para a função trabalhar
+            conversor_moeda(valor_usuario, escolha_origem, escolha_destino)
+
+        except ValueError:
+            print("Ops! Você precisa digitar um número válido.")
+        except KeyError:
+            print("Erro: Código de moeda não encontrado no sistema.")
